7680cc23ee2ef81a4267596fff0c3dc2d841abb4
yuchian <violin1996@gmail.com>
2015-10-19 00:01:48 +0800

    Date Menu Done git status

--- todolistgui.py
+++ todolistgui.py
@@ -11,18 +11,11 @@ class ToDoListGUI(Frame):
         
     def createWidgets(self):
         self.timeText = Label(self)
-        self.timeText["text"] = "Time:"
+        self.timeText["text"] = "Dead Line:"
         self.timeText.grid(row=0, column=0)
 
-        self.monthVal = StringVar()
-        self.monthVal.set("Year")
-        self.monthMenu = Menubutton(self, text="Year", textvariable=self.monthVal)
-        self.monthMenu.grid(row=0, column=1, columnspan=3)
-        self.month = Menu(self.monthMenu, tearoff=0)
-        self.monthMenu["menu"] = self.month
-        self.month.add_radiobutton(label="1", variable=self.monthVal, value="1")
-        self.month.add_radiobutton(label="2", variable=self.monthVal, value="2")
-        #self.makeMonthMenu()
+        self.makeMonthMenu()
+        self.makeDateMenu()
 
 
         self.todoText = Label(self)
@@ -49,7 +42,30 @@ class ToDoListGUI(Frame):
         self.todoFrame.columnconfigure(0, weight=1)
         self.todoList = Listbox(self.todoFrame)
         self.todoList.grid(sticky=N)
-
+    
+    def makeMonthMenu(self):
+        self.monthVal = StringVar()
+        self.monthVal.set("Month")
+        self.monthMenu = Menubutton(self, text="Month", textvariable=self.monthVal)
+        self.monthMenu.grid(row=0, column=1, columnspan=3)
+        self.month = Menu(self.monthMenu, tearoff=0)
+        self.monthMenu["menu"] = self.month
+        for i in range(1,13):
+            self.month.add_radiobutton(label=str(i), variable=self.monthVal, value=str(i), command=self.whenMonthMenubuttonClicked)
+  
+    def makeDateMenu(self):
+        self.dateVal = StringVar()
+        self.dateVal.set("Date")
+        self.DateMenu = Menubutton(self, text="Date", textvariable=self.dateVal)
+        self.DateMenu.grid(row=0, column=2, columnspan=3)
+        self.Date = Menu(self.DateMenu, tearoff=0)
+        self.DateMenu["menu"] = self.Date
+        
+        date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        
+        if self.monthVal.get() != "Month":
+            for i in range(1,date[int(self.monthVal.get())-1]+1):
+                self.Date.add_radiobutton(label=str(i), variable=self.dateVal, value=str(i))
     def addMethod(self):
         self.todoList.insert(0, self.timeField.get() + "\n" + self.todoField.get())
         self.todoCnt+=1
@@ -65,6 +81,9 @@ class ToDoListGUI(Frame):
             return
 
         self.todoList.delete(selection[0])
+
+    def whenMonthMenubuttonClicked(self):
+        self.makeDateMenu()
 
 if __name__=='__main__':
     root = Tk()

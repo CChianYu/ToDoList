f6f5044a27139f316b027997655459750a2fe719
yuchian <violin1996@gmail.com>
2015-10-19 21:07:45 +0800

    could be more desired

--- todolistgui.py
+++ todolistgui.py
@@ -10,14 +10,16 @@ class ToDoListGUI(Frame):
         self.todoCnt = 1
         
     def createWidgets(self):
-        self.timeText = Label(self)
-        self.timeText["text"] = "Dead Line:"
-        self.timeText.grid(row=0, column=0)
 
+        #Dead Line:
+        self.timeText = Label(self, text="Dead Line:")
+        self.timeText.grid(row=0, column=0)
         self.makeMonthMenu()
         self.makeDateMenu()
+        self.makeHourMenu()
+        self.makeMinuteMenu()
 
-
+        #ToDo:
         self.todoText = Label(self)
         self.todoText["text"] = "ToDo:"
         self.todoText.grid(row=1, column=0)
@@ -25,29 +27,33 @@ class ToDoListGUI(Frame):
         self.todoField["width"] = 50
         self.todoField.grid(row = 1, column=1, columnspan=6)
 
+        #Add Button
         self.add = Button(self, command=self.addMethod)
         self.add["text"] = "Add"
         self.add.grid(row=2, column=0)
 
+        #Remove Button
         self.remove = Button(self, text="Remove", command=self.removeMethod)
         self.remove.grid(row=2, column=1)
 
+        #prompt message
         self.displayText = Label(self)
-        self.displayText["text"] = "Message"
+        self.displayText["text"] = ""
         self.displayText.grid(row=4, column=0, columnspan=7)
 
+        #All To Do
         self.todoFrame = LabelFrame(self)
         self.todoFrame["text"] = "To Do List"
-        self.todoFrame.grid(row=3, columnspan=7, sticky=N)
+        self.todoFrame.grid(row=3, columnspan=7)
         self.todoFrame.columnconfigure(0, weight=1)
-        self.todoList = Listbox(self.todoFrame)
-        self.todoList.grid(sticky=N)
+        self.todoList = Listbox(self.todoFrame, width=50)
+        self.todoList.grid()
     
     def makeMonthMenu(self):
         self.monthVal = StringVar()
         self.monthVal.set("Month")
         self.monthMenu = Menubutton(self, text="Month", textvariable=self.monthVal)
-        self.monthMenu.grid(row=0, column=1, columnspan=3)
+        self.monthMenu.grid(row=0, column=1)
         self.month = Menu(self.monthMenu, tearoff=0)
         self.monthMenu["menu"] = self.month
         for i in range(1,13):
@@ -57,7 +63,7 @@ class ToDoListGUI(Frame):
         self.dateVal = StringVar()
         self.dateVal.set("Date")
         self.DateMenu = Menubutton(self, text="Date", textvariable=self.dateVal)
-        self.DateMenu.grid(row=0, column=2, columnspan=3)
+        self.DateMenu.grid(row=0, column=2)
         self.Date = Menu(self.DateMenu, tearoff=0)
         self.DateMenu["menu"] = self.Date
         
@@ -66,10 +72,46 @@ class ToDoListGUI(Frame):
         if self.monthVal.get() != "Month":
             for i in range(1,date[int(self.monthVal.get())-1]+1):
                 self.Date.add_radiobutton(label=str(i), variable=self.dateVal, value=str(i))
+
+    def makeHourMenu(self):
+        self.hourVal = StringVar()
+        self.hourVal.set("Hour")
+        self.hourMenu = Menubutton(self, text="Hour", textvariable=self.hourVal)
+        self.hourMenu.grid(row=0, column=3)
+        self.hour = Menu(self.hourMenu)
+        self.hourMenu["menu"] = self.hour
+
+        for i in range(0, 24):
+            self.hour.add_radiobutton(label=str(i), variable=self.hourVal, value=str(i))
+
+    def makeMinuteMenu(self):
+        self.minuteVal = StringVar()
+        self.minuteVal.set("Minute")
+        self.minuteMenu = Menubutton(self, text="Minute", textvariable=self.minuteVal)
+        self.minuteMenu.grid(row=0, column=4)
+        self.minute = Menu(self.minuteMenu)
+        self.minuteMenu["menu"] = self.minute
+        for i in range(0, 60, 30):
+            self.minute.add_radiobutton(label=str(i), variable=self.minuteVal, value=str(i))
+
     def addMethod(self):
-        self.todoList.insert(0, self.timeField.get() + "\n" + self.todoField.get())
-        self.todoCnt+=1
+
+        #check if users done the form
+        if self.monthVal.get()=="Month" or self.dateVal.get()=="Date" or self.hourVal.get()=="Hour" or self.minuteVal.get()=="Minute" or self.todoField.get()=="":
+            self.displayText["text"] = "You need to complete the up forms first. ^^"
+            return
+        
+        #add a task to my todoListbox
+        todoText = "Dead Line: " + self.monthVal.get() + "/" + self.dateVal.get() + " " + self.hourVal.get() + ":" + self.minuteVal.get() + " Task: " + self.todoField.get()
+        self.todoList.insert(0, todoText)
         self.displayText["text"] = "Add to ToDoList!!"
+
+        #clear forms
+        self.monthVal.set("Month")
+        self.dateVal.set("Date")
+        self.hourVal.set("Hour")
+        self.minuteVal.set("Minute")
+        self.todoField.delete(0, 50)
     
     def removeMethod(self):
         selection = self.todoList.curselection()
@@ -80,9 +122,11 @@ class ToDoListGUI(Frame):
             self.displayText["text"] = "Choose a ToDo task to remove."
             return
 
+        self.displayText["text"] = "Remove " + self.todoList.get(selection[0])
         self.todoList.delete(selection[0])
 
     def whenMonthMenubuttonClicked(self):
+        self.DateMenu.destroy()
         self.makeDateMenu()
 
 if __name__=='__main__':
